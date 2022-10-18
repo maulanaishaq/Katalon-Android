d79399b90c1d63e863514ca6b122b142cbf038f2
Maulana Ishaq <ishaqlanaa17@gmail.com>
2022-10-18 13:21:12 +0700

    add test case failed login

---
+++ Object Repository/Login Page/txt_ErrorMessage.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<MobileElementEntity>
+   <description></description>
+   <name>txt_ErrorMessage</name>
+   <tag></tag>
+   <elementGuidId>1e022687-ce95-4bce-a787-5c9711b1fb12</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//android.view.ViewGroup[@content-desc=&quot;test-Error message&quot;]/android.widget.TextView</value>
+      <webElementGuid>6e08af56-80b4-4b5f-b97d-c64e206f7f4b</webElementGuid>
+   </webElementProperties>
+   <locator>//android.view.ViewGroup[@content-desc=&quot;test-Error message&quot;]/android.widget.TextView</locator>
+   <locatorStrategy>ATTRIBUTES</locatorStrategy>
+</MobileElementEntity>
--- Scripts/Failed Login/Script1666068858250.groovy
+++ Scripts/Failed Login/Script1666068858250.groovy
@@ -3,8 +3,8 @@ import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint
-import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.configuration.RunConfiguration as RunConfiguration
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
@@ -26,11 +26,15 @@ RunConfiguration.setMobileDriverPreferencesProperty('appActivity', 'com.swaglabs
 
 Mobile.startApplication(appFile, false)
 
-Mobile.setText(findTestObject('Login Page/txt_Username'), 'standard_user', 0)
+Mobile.setText(findTestObject('Login Page/txt_Username'), 'locked_out_user', 0)
 
 Mobile.setText(findTestObject('Login Page/txt_Password'), 'secret_sauce', 0)
 
 Mobile.tap(findTestObject('Login Page/btn_Login'), 0)
 
+Mobile.verifyElementExist(findTestObject('Login Page/txt_ErrorMessage'), 0)
 
+String actual = Mobile.getText(findTestObject('Login Page/txt_ErrorMessage'), 0)
+
+Mobile.verifyMatch(actual, 'Sorry, this user has been locked out.', false)
 
